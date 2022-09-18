39c9077fc3f0580179c3b2e266d4f28926fd48f2
Nate Bouchat <nab46@students.uwf.edu>
2022-09-17 23:49:14 -0500

    Player movement + better colision

--- TetrisBotAttack/src/BlockControl.cs
+++ TetrisBotAttack/src/BlockControl.cs
@@ -4,19 +4,29 @@ using System;
 public class BlockControl : Sprite
 {
     
-    Vector2 Velocity;
+    Vector2 down1;
+    Vector2 right1;
     Vector2 startPosition;
 
     public BlockControl()
     {
-        Velocity = new Vector2(0, 40);
+        down1 = new Vector2(0, 40);
+        right1 = new Vector2(40, 0);
         startPosition = new Vector2(-20, -340);
         Position = startPosition;
     }
 
-    public void blockMove(float timer)
+    public void blockMoveDown()
     {
-          this.Position += Velocity;
+        this.Position += down1;
+    }
+
+    public void blockMoveRight() {
+            this.Position += right1;
+    }
+
+    public void blockMoveLeft() {
+            this.Position -= right1;
     }
 
     public float getYPos() {
--- TetrisBotAttack/src/tetrisBoard.cs
+++ TetrisBotAttack/src/tetrisBoard.cs
@@ -2,16 +2,21 @@ using Godot;
 using System;
 
 public class tetrisBoard : Node2D
-{   float timer;
-    int tickTime;
-    int[,] board;
-    BlockControl block;
+{   private float timer;
+    private int[,] board;
+    private BlockControl block;
+    private Vector2 _screenSize;
+    private bool downActionBuffer;
     
+    [Export]
+    public int tickTime = 10;
     [Export]
     public PackedScene DownedBlocks;
 
     public override void _Ready()
     {
+        _screenSize = GetViewport().Size;
+
         board = new int[10, 20];
         for(int i = 0; i < 20; i++) {
             for(int j = 0; j < 10; j++) {
@@ -21,35 +26,93 @@ public class tetrisBoard : Node2D
 
         block = GetChild<BlockControl>(0);
         timer = 0;
-        tickTime = 5;
+        downActionBuffer = false;
     }
 
     public override void _Process(float delta)
     {
+        playerInput();
+
         timer += 1;
         if (timer >= tickTime) {
             timer -= tickTime;
-            block.blockMove(timer);
+            block.blockMoveDown();
         }
-        //if(block.getYPos() == 760.0) {
-        if(hasCollided( block.getXPos(), block.getYPos()) == true) {
-            board[((int)block.getXPos()/40), ((int)block.getYPos()/40)] = 1;
-            createInstance(block.getXPos(), block.getYPos());
-            block.resetBlock();
+
+        hasCollidedDown(block.getXPos(), block.getYPos());
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void playerInput() {
+        if (Input.IsActionJustPressed("ui_right"))
+        {
+            if(hascollidedSide('r') == false) {
+                block.blockMoveRight();
+            }
+        }
+        if (Input.IsActionJustPressed("ui_left"))
+        {
+            if(hascollidedSide('l') == false) {
+                block.blockMoveLeft();
+            }
+        }
+        if (Input.IsActionPressed("ui_down")) {
+            downActionBuffer = true;
+            if(hasCollidedDown(block.getXPos(), block.getYPos()) == false) {
+                if(timer%3 == 0) {
+                    block.blockMoveDown();
+                }
+            }
         }
     }
 
     private void createInstance(float lockedPosX, float lockedPosY) {
-        var DownedBlockInstance = (Sprite)DownedBlocks.Instance();
-        AddChild(DownedBlockInstance);
-        DownedBlockInstance.Position = new Vector2(lockedPosX - 180, lockedPosY - 380);
+        if(downActionBuffer == false) {
+            board[((int)lockedPosX/40), ((int)lockedPosY/40)] = 1;
+
+            var DownedBlockInstance = (Sprite)DownedBlocks.Instance();
+            AddChild(DownedBlockInstance);
+            DownedBlockInstance.Position = new Vector2(lockedPosX - 180, lockedPosY - 380);
+        
+            block.resetBlock();
+        }
     }
 
-    private bool hasCollided(float xPos, float yPos) {
-        if(yPos == 760) {
+    private bool hasCollidedDown(float xPos, float yPos) {
+        if(yPos > 760) {
+            createInstance(block.getXPos(), 760);
+            return false;
+        } 
+        else if(yPos == 760) {
+            createInstance(block.getXPos(), block.getYPos());
             return true;
         }
+        else if (board[((int)block.getXPos()/40), ((int)block.getYPos()/40)] == 1) {
+            createInstance(block.getXPos(), block.getYPos()-40);
+            return true;    
+        }
         else if (board[((int)block.getXPos()/40), ((int)block.getYPos()/40)+1] == 1) {
+            createInstance(block.getXPos(), block.getYPos());
+            return true;    
+        }
+        else {
+            downActionBuffer = false;
+            return false;
+        }
+    }
+
+    private bool hascollidedSide(char side) {
+        if(side == 'r' && block.getXPos() == 360) {
+            return true;
+        }
+        else if(side == 'r' && board[((int)block.getXPos()/40)+1, ((int)block.getYPos()/40)] == 1) {
+            return true;
+        }
+        else if(side == 'l' && block.getXPos() == 0) {
+            return true;
+        }
+        else if (side == 'l' && board[((int)block.getXPos()/40)-1, ((int)block.getYPos()/40)] == 1) {
             return true;
         }
         else {
