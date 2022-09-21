7f76a976c3e66e01beb922bf277134a39d935e64
Nate Bouchat <nab46@students.uwf.edu>
2022-09-20 22:22:30 -0500

    can now clear lines

--- TetrisBotAttack/src/tetrisBoard.cs
+++ TetrisBotAttack/src/tetrisBoard.cs
@@ -3,9 +3,11 @@ using System;
 
 public class tetrisBoard : Node2D
 {   private float timer;
+    private int clearDelay;
     private Sprite[,] board;
     private BlockControl block;
     private Vector2 _screenSize;
+    private Vector2 blocksDown1;
     private bool collisionDetectedY;
     private bool collisionDetectedX;
     
@@ -26,7 +28,9 @@ public class tetrisBoard : Node2D
         }
 
         block = GetChild<BlockControl>(0);
+        blocksDown1 = new Vector2(0, -40);
         timer = 0;
+        clearDelay = 0;
     }
 
     public override void _Process(float delta)
@@ -36,11 +40,14 @@ public class tetrisBoard : Node2D
             playerInput();
 
             timer += 1;
-            if (timer >= tickTime) {
-                timer -= tickTime;
+            if (timer >= tickTime + clearDelay) {
+                timer = timer - (tickTime + clearDelay);
+                clearDelay = 0;
+                pushDownOverEmpty();
                 block.blockMoveDown();
             }
             hasCollidedDown(block.getBlockPos());
+            completedLine();
         }
     }
 
@@ -49,25 +56,27 @@ public class tetrisBoard : Node2D
     private void playerInput() {
         if (Input.IsActionJustPressed("ui_right"))
         {
-            if(hasCollidedSide('r') == false) {
-                block.blockMoveRight();
+            block.blockMoveRight();
+            if(hasCollided(block.getBlockPos()) == true) {
+                block.blockMoveLeft();
             }
         }
         if (Input.IsActionJustPressed("ui_left"))
         {
-            if(hasCollidedSide('l') == false) {
-                block.blockMoveLeft();
+            block.blockMoveLeft();
+            if(hasCollided(block.getBlockPos()) == true) {
+                block.blockMoveRight();
             }
         }
         if (Input.IsActionJustPressed("rotateLeft")) {
             block.rotateBlocks('l');
-            if(hasCollidedRotate(block.getBlockPos()) == true) {
+            if(hasCollided(block.getBlockPos()) == true) {
                 block.rotateBlocks('r');
             }
         }
         if (Input.IsActionJustPressed("rotateRight")) {
             block.rotateBlocks('r');
-            if(hasCollidedRotate(block.getBlockPos()) == true) {
+            if(hasCollided(block.getBlockPos()) == true) {
                 block.rotateBlocks('l');
             }
         }
@@ -86,17 +95,20 @@ public class tetrisBoard : Node2D
     }
 
     private void createInstance(float[,] blockPos, float adjustment) {
-            for(int i = 0; i < 4; i++) {
-                if((blockPos[1,i]+adjustment)/40 > 0 && (blockPos[1,i]+adjustment)/40 < 40) {
+        for(int i = 0; i < 4; i++) {
+            if((blockPos[1,i]+adjustment)/40 > 0 && (blockPos[1,i]+adjustment)/40 < 40) {
+                if(board[((int)blockPos[0,i]/40), ((int)blockPos[1, i]/40)] == null) {
                     Sprite DownedBlockInstance = (Sprite)DownedBlocks.Instance();
                     AddChild(DownedBlockInstance);
                     DownedBlockInstance.Position = new Vector2(blockPos[0,i] - 180, blockPos[1,i] - 380 + adjustment);
                     board[((int)blockPos[0,i]/40), ((int)(blockPos[1,i]+adjustment)/40)] = DownedBlockInstance;
                 }
             }
-        
-            block.resetBlock();
+        }
+
+        block.resetBlock();
     }
+
     private bool hasCollidedDown(float[,] blockPos) {
         collisionDetectedY = false;
         for(int i = 0; i < 4; i++) { 
@@ -125,28 +137,7 @@ public class tetrisBoard : Node2D
         return collisionDetectedY;
     }
 
-    private bool hasCollidedSide(char side) {
-        collisionDetectedX = false;
-        for(int i = 0; i < 4; i++) {
-            if(side == 'r' && block.getBlockPos()[0,i] == 360) {
-                collisionDetectedX = true;
-            }
-            else if(side == 'r' && board[((int)block.getBlockPos()[0,i]/40)+1, ((int)block.getBlockPos()[1,i]/40)] != null) {
-                collisionDetectedX = true;
-            }
-            else if(side == 'l' && block.getBlockPos()[0,i] == 0) {
-                collisionDetectedX = true;
-            }
-            else if (side == 'l' && board[((int)block.getBlockPos()[0,i]/40)-1, ((int)block.getBlockPos()[1,i]/40)] != null) {
-                collisionDetectedX = true;
-            }
-            else {
-            }
-        }
-        return collisionDetectedX;
-    }
-
-    private bool hasCollidedRotate(float[,] blockPos) {
+    private bool hasCollided(float[,] blockPos) {
         for(int i = 0; i < 4; i++) {
             if(blockPos[0, i] < 0 || blockPos[0, i] > 360 || blockPos[1, i] < 0 || blockPos[1, i] > 760) {
                 return true;
@@ -157,4 +148,43 @@ public class tetrisBoard : Node2D
         }
         return false;
     }
+
+    private bool completedLine() {
+        for(int i = 0; i < 20; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(board[j, i] == null) {
+                    break;
+                }
+                if(j == 9) {
+                    for(int k = 0; k < 10; k++) {
+                        RemoveChild(board[k, i]);
+                        board[k, i] = null;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void pushDownOverEmpty() {
+        for(int i = 0; i < 20; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(board[j, i] != null) {
+                    break;
+                }
+                if(j == 9) {
+                    for(int k = i-1; k >= 0; k--) {
+                        for(int n = 0; n < 10; n++) {
+                            if(board[n, k] != null) {
+                                board[n, k].Position -= blocksDown1;
+                                board[n, k+1] = board[n, k];
+                                board[n, k] = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } 
+
 }
