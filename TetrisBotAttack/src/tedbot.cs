fb3cb3215bbd1c74d830d390c94c983652117203
Nate Bouchat <nab46@students.uwf.edu>
2022-10-06 20:48:27 -0500

    Tedbot hats setup

--- TetrisBotAttack/src/tedbot.cs
+++ TetrisBotAttack/src/tedbot.cs
@@ -3,20 +3,72 @@ using System;
 
 public class tedbot : Control
 {
-    AnimationPlayer walkIn;
-    AnimatedSprite TedbotSP;
+    private AnimationPlayer anim;
+    private AnimatedSprite TedbotSP;
+    private Sprite hat;
+    private Directory hatFolder;
+    private String hatName;
+    private String[] allHats;
 
     public override void _Ready()
     {
-        walkIn = GetNode<AnimationPlayer>("AnimationPlayer");
+        anim = GetNode<AnimationPlayer>("AnimationPlayer");
         TedbotSP = GetNode<AnimatedSprite>("AnimatedSprite");
+        hatFolder = new Directory();
+        initializeAllhats();
+        printHats();
+        //hat = GetNode<Sprite>("AnimatedSprite/hat");
+        //hat.Texture = (Texture)(ResourceLoader.Load("res://assets/hats/hat_block.png"));
         startingAnimations();
     }
 
     public async void startingAnimations() {
-        walkIn.Play("TedWalkIn");
-        await ToSignal(walkIn, "animation_finished");
+        anim.Play("TedWalkIn");
+        await ToSignal(anim, "animation_finished");
         TedbotSP.Animation = "Idle";
+        //anim.Play("TedHatIdle");
+
+    }
+
+    private void initializeAllhats() { 
+        if(hatFolder.Open("res://assets/hats") == Error.Ok) {
+            hatFolder.ListDirBegin();
+            int n = 0;
+            String fileName = hatFolder.GetNext();
+            while(fileName != "") {
+                if(fileName.EndsWith(".png")) {
+                    n++;
+                }
+                fileName = hatFolder.GetNext();
+            }
+            allHats = new String[n+1];
+            allHats[0] = "No hat";
+            int i = 1;
+            hatFolder.ListDirBegin();
+            fileName = hatFolder.GetNext();
+            while(fileName != "") {
+                if(fileName.EndsWith(".png")) {
+                    allHats[i] = fileName.Remove(fileName.Length - 4);
+                    i++;
+                }
+                fileName = hatFolder.GetNext();
+            }
+        }
+        else {
+            GD.Print("Folder not found in GetAllhats");
+        }
+    }
+    
+    //Testing Only
+    private void printHats () {
+        for(int i = 0; i < allHats.Length; i++) {
+            GD.Print(allHats[i]);
+        }
+    }
+
+    public String HatName {
+        get {return hatName;}
+        set {hatName = value;}
     }
 
 }
